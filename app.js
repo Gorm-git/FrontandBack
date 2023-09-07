24d409a307817c99bc158e1670749ed0b0a5871d
Gorm-git <124032495+Gorm-git@users.noreply.github.com>
2023-09-07 13:47:32 +0200

    Get-post-id

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express, { request, response } from "express";
 import fs from "fs/promises";
 import cors from "cors";
+import { get } from "http";
 
 const app = express();
 const port = 5000;
@@ -15,6 +16,16 @@ app.listen(port, () => {
   console.log("42");
 });
 
+// For at gøre det nemmere for mig selv så kan jeg lave en funktion
+// Som ligger listen i en konstant "artist", herfra vil jeg
+// kunne genbruge den i  mine routers
+async function getArtistList() {
+  const list = await fs.readFile("artistlist.json");
+  const artist = JSON.parse(list);
+
+  return artist;
+}
+
 // Så Laver jeg en get, altså et response sådan hjemmesiden kan indlæses
 app.get("/", (request, response) => {
   response.send("The website now GET's it");
@@ -23,9 +34,23 @@ app.get("/", (request, response) => {
 //Der skal være en funktion som indlæser JSON filen samt vent
 
 //jeg laver et get kald som henter hele listen:
-app.get("/artists", async (request, response) => {
-  const artistList = await fs.readFile("artistlist.json");
-  const artist = JSON.parse(artistList);
+app.get("/artists", getAllArtists);
 
-  response.json(artist);
-});
+// så laver jeg et get kald som kan hente specifikke artists baseret på deres
+app.get("/artists/:id", getOneArtistID);
+
+// Jeg laver nu et post response, som requester bodyen fra det nye object som,
+// er blevet oprettet, dernæst når den har fået artists bodien så skal den give
+//
+
+async function getAllArtists(response) {
+  response.json(await getArtistList());
+}
+
+async function getOneArtistID(request, response) {
+  const id = request.params.id;
+  const artistList = await getArtistList();
+  const artistID = artistList.find((artistID) => artistID.id === id);
+
+  response.json(artistID);
+}
