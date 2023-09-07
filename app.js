e643748b0019a9ffcb10b1e2383db9c2b06fe9ea
Gorm-git <124032495+Gorm-git@users.noreply.github.com>
2023-09-07 16:06:44 +0200

    Delete

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ async function getArtistList() {
   return artist;
 }
 
+/*--------------ROUTES--------------*/
+
 // Så Laver jeg en get, altså et response sådan hjemmesiden kan indlæses
 app.get("/", (request, response) => {
   response.send("The website now GET's it");
@@ -42,20 +44,14 @@ app.get("/artists/:id", getOneArtistID);
 // det nye objekt et id baseret på det specifikke milisekund det blev oprettet
 app.post("/artists", postNewArtist);
 
-// Nu laver jeg en delete route. Jeg går
-app.delete("/artists/:id", async (request, response) => {
-  const id = request.params.id;
-  const artistList = await getArtistList();
-  const index = artistList.findIndex((artist) => artist.id === id);
+// Nu laver jeg en delete route. Den skal sende en fejlbesked hvis en artist,
+// med dette id ikke kan findes. Hvis at denne artist eksisterer så bliver
+// denne artist fjernet fra listen, og dernæst bliver listen rettet
+app.delete("/artists/:id", deleteArtist);
 
-  if (index === -1) {
-    response.status(404).json({ message: "Artist not found" });
-  } else {
-    artistList.splice(index, 1);
+//
 
-    response.json(await getArtistList());
-  }
-});
+/*--------------FUNKTIONER--------------*/
 
 async function getAllArtists(request, response) {
   response.json(await getArtistList());
@@ -79,4 +75,18 @@ async function postNewArtist(request, response) {
   artistList.push(newArtist);
   fs.writeFile("artistlist.json", JSON.stringify(artistList));
   response.json(artistList);
+}
+
+async function deleteArtist(request, response) {
+  const id = request.params.id;
+  const artistList = await getArtistList();
+  const index = artistList.findIndex((artist) => artist.id === id);
+
+  if (index === -1) {
+    response.status(404).json({ message: "Artist not found" });
+  } else {
+    artistList.splice(index, 1);
+
+    response.json(await getArtistList());
+  }
 }
