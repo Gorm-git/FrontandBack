e99a8f0fc6184b66bda6939d13399ae36268f9b7
Gorm-git <124032495+Gorm-git@users.noreply.github.com>
2023-09-07 23:56:11 +0200

    Update routeFuncs.js

--- backend/routeFuncs.js
+++ backend/routeFuncs.js
@@ -1,72 +1,72 @@
-async function getAllArtists(request, response) {
-  response.json(await getArtistList());
-}
+// async function getAllArtists(request, response) {
+//   response.json(await getArtistList());
+// }
 
-async function getOneArtistID(request, response) {
-  const id = Number(request.params.id);
-  const artistList = await getArtistList();
-  const artistID = artistList.find((artistID) => artistID.id === id);
+// async function getOneArtistID(request, response) {
+//   const id = Number(request.params.id);
+//   const artistList = await getArtistList();
+//   const artistID = artistList.find((artistID) => artistID.id === id);
 
-  response.json(artistID);
-}
+//   response.json(artistID);
+// }
 
-async function postNewArtist(request, response) {
-  const newArtist = request.body;
-  console.log(request.body);
-  newArtist.id = new Date().getTime();
-  newArtist.favorite = false;
+// async function postNewArtist(request, response) {
+//   const newArtist = request.body;
+//   console.log(request.body);
+//   newArtist.id = new Date().getTime();
+//   newArtist.favorite = false;
 
-  console.log(newArtist);
+//   console.log(newArtist);
 
-  const artistList = await getArtistList();
-  artistList.push(newArtist);
-  fs.writeFile("artistlist.json", JSON.stringify(artistList));
-  response.json(artistList);
-}
+//   const artistList = await getArtistList();
+//   artistList.push(newArtist);
+//   fs.writeFile("artistlist.json", JSON.stringify(artistList));
+//   response.json(artistList);
+// }
 
-async function deleteArtist(request, response) {
-  const id = Number(request.params.id);
-  const artistList = await getArtistList();
-  const index = artistList.findIndex((artist) => artist.id === id);
+// async function deleteArtist(request, response) {
+//   const id = Number(request.params.id);
+//   const artistList = await getArtistList();
+//   const index = artistList.findIndex((artist) => artist.id === id);
 
-  if (index === -1) {
-    response.status(404).json({ message: "Artist not found" });
-  } else {
-    artistList.splice(index, 1);
+//   if (index === -1) {
+//     response.status(404).json({ message: "Artist not found" });
+//   } else {
+//     artistList.splice(index, 1);
 
-    fs.writeFile("artistlist.json", JSON.stringify(artistList));
-    response.json(artistList);
-  }
-}
+//     fs.writeFile("artistlist.json", JSON.stringify(artistList));
+//     response.json(artistList);
+//   }
+// }
 
-async function updateAnArtist(request, response) {
-  console.log(request.params.id);
-  const id = Number(request.params.id);
-  console.log(id);
+// async function updateAnArtist(request, response) {
+//   console.log(request.params.id);
+//   const id = Number(request.params.id);
+//   console.log(id);
 
-  const artistList = await getArtistList();
+//   const artistList = await getArtistList();
 
-  const updatingArtist = artistList.find((artist) => artist.id === id);
+//   const updatingArtist = artistList.find((artist) => artist.id === id);
 
-  const body = request.body;
+//   const body = request.body;
 
-  updatingArtist.name = body.name;
-  updatingArtist.birthdate = body.birthdate;
-  updatingArtist.activeSince = body.activeSince;
-  updatingArtist.genres = body.genres;
-  updatingArtist.labels = body.labels;
-  updatingArtist.website = body.website;
-  updatingArtist.image = body.image;
-  updatingArtist.shortDescription = body.shortDescription;
+//   updatingArtist.name = body.name;
+//   updatingArtist.birthdate = body.birthdate;
+//   updatingArtist.activeSince = body.activeSince;
+//   updatingArtist.genres = body.genres;
+//   updatingArtist.labels = body.labels;
+//   updatingArtist.website = body.website;
+//   updatingArtist.image = body.image;
+//   updatingArtist.shortDescription = body.shortDescription;
 
-  fs.writeFile("artistlist.json", JSON.stringify(artistList));
-  response.json(artistList);
-}
+//   fs.writeFile("artistlist.json", JSON.stringify(artistList));
+//   response.json(artistList);
+// }
 
-export {
-  getAllArtists,
-  getOneArtistID,
-  postNewArtist,
-  deleteArtist,
-  updateAnArtist,
-};
+// export {
+//   getAllArtists,
+//   getOneArtistID,
+//   postNewArtist,
+//   deleteArtist,
+//   updateAnArtist,
+// };
